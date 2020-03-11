5168b9493e64d25b28573e9481964dd46f5a74ac
EraAmate <valeriiaera@gmail.com>
2020-03-11 16:43:47 +0100

    Add Router and create pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Add from "./components/pages/Add";
+import Vote from "./components/pages/Vote";
+import Result from "./components/pages/Result";
 
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <Router>
+        <Header />
+
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <Add />
+            </Route>
+            <Route path="/vote">
+              <Vote />
+            </Route>
+            <Route path="/result">
+              <Result />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
+    </>
   );
 }
 
