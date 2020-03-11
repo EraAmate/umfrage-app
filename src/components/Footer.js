74102833cf51d0d379ee51d1c428d326f9a63bed
EraAmate <valeriiaera@gmail.com>
2020-03-11 19:32:23 +0100

    Add footer with button to all pages

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <button className="btn">Next</button>
+    </footer>
+  );
+}
+export default Footer;
