c2349575dd4a0e3cad0f065383fc5a7ac5ce528c
EraAmate <60475379+EraAmate@users.noreply.github.com>
2020-03-16 06:46:16 +0100

    Results page (#14)
    * Complete results page
    
    * "Rename results path"

--- src/components/pages/Result.js
+++ src/components/pages/Result.js
@@ -1,10 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Card from "../Card";
+import { useParams } from "react-router-dom";
+
+const POLLS_API_URL =
+  process.env.REACT_APP_API_POLLS ||
+  "https://my-json-server.typicode.com/EraAmate/umfrage-app/polls";
 
 function Result() {
+  const { pollId } = useParams();
+  const [poll, setPoll] = React.useState(null);
+
+  React.useEffect(() => {
+    async function getPoll() {
+      const response = await fetch(`${POLLS_API_URL}/${pollId}`);
+      const poll = await response.json();
+      setPoll(poll);
+    }
+
+    getPoll();
+  }, [pollId]);
+
   return (
     <Card>
+      <h2>{poll?.question}</h2>
+      <div>{poll?.answerOne}</div>
+      <div>{poll?.answerTwo}</div>
+      <div>{poll?.answerThree}</div>
       <Link to="/">Add!</Link>
     </Card>
   );
